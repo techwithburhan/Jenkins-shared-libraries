2d6d8baca08b67ae4aecccc8b8a7765e1094a76b
Burhan <techdeployers.com@gmail.com>
2026-02-22 02:07:30 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,1 +1,17 @@
+def call(String imageName, String tag, String buildContext = '.') {
 
+    if (!imageName?.trim()) {
+        error "Image name is required!"
+    }
+
+    if (!tag?.trim()) {
+        tag = "latest"
+    }
+
+    echo "Building Docker Image: ${imageName}:${tag}"
+    echo "Build Context: ${buildContext}"
+
+    sh """
+        docker build -t ${imageName}:${tag} ${buildContext}
+    """
+}
